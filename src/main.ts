54565107d9ffc4299b2b9b6c9f3d0f94011930b3
Justin Watkinson <justin.watkinson@gmail.com>
2022-04-08 15:27:57 -0500

    End of training

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
+import * as git from './git'
+import * as github from './github'
+import { GitHub } from '@actions/github/lib/utils'
 //import {wait} from './wait'
 
 // I added the export to the function to get rid of some of the tests
 export async function run(): Promise<void> {
   try {
-    core.setOutput('release-url', 'https://example.com')
+    const token = core.getInput('repo-token')
+
+    const tag = event.getCreatedTag()
+    var releaseUrl = ''
+
+    if (tag && version.isSemVer(tag)) { // null check on the tag object first, short-circuit operator
+      const changelog = await git.getChangesIntroducedByTag(tag)
+
+      releaseUrl = await github.createReleaseDraft(tag, token, changelog)
+    }
+
+    core.setOutput('release-url', releaseUrl)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
-
 
 run()
---
+++ src/markdown.ts
@@ -0,0 +1,8 @@
+// This function magically appeared  in the videos, couldn't find an implementation, probably not important
+// Should be updated to split the string into an array perhaps?  We'll have to see it in action (pun intended)
+
+export function toUnorderedList(
+    foo: string
+): string {
+    return foo
+}
